5e0fd2b7afabdca59ad49dfa9e44830bc4a7088d
Diogo Simao <diogo.simao98@hotmail.com>
2020-02-13 19:37:25 +0000

    Alterações nos estilos e remoção de parametros duplicados

--- espaco/forms.py
+++ espaco/forms.py
@@ -9,11 +9,11 @@ TIPO_SALA = [
 ]
 
 class createRoomForm(forms.Form):
-    nome = forms.CharField(max_length=4,required=True,label = "Nome da Sala: ")
+    nome = forms.CharField(max_length=4,required=True)
     piso = forms.IntegerField(min_value=-1,max_value=2)
-    lotacao = forms.IntegerField(min_value=10,max_value=55,label = "Lotação Máxima")
-    tipo = forms.CharField(widget=forms.SelectMultiple(choices=TIPO_SALA),required=True, label = "Tipo")
-    status = forms.ChoiceField(choices=((0,'Indisponivel'),(1,'Disponivel')),required=True, label = "Disponibilidade",initial=1)
+    lotacao = forms.IntegerField(min_value=10,max_value=55)
+    tipo = forms.CharField(widget=forms.SelectMultiple(choices=TIPO_SALA),required=True)
+    status = forms.ChoiceField(choices=((0,'Indisponivel'),(1,'Disponivel')),required=True,initial=1)
 
         
 class createRoomModelForm(forms.ModelForm):
@@ -24,7 +24,7 @@ class createRoomModelForm(forms.ModelForm):
     def clean_lotacao(self):
         cleaned_data = super().clean()
         lotacao = cleaned_data.get('lotacao')
-       
+    
         if lotacao < 1 or lotacao > 55:
             raise forms.ValidationError("Erro: Lotação tem de ser positiva e até 55.")
         
--- horario/views.py
+++ horario/views.py
@@ -5,9 +5,10 @@ from keycode.source.models import Utilizador, Acesso
 
 def horario(request):
     username = request.user
+    print(username)
     current_user = Utilizador.objects.get(username=username)
-    acesso = Acesso.objects.get(utilizador=current_user)
-
-    print(acesso)
+    #acesso = Acesso.objects.get(utilizador=current_user.id)
+    #for item in Acesso.objects.get(utilizador=current_user.id):
+       # print(item[0].id)
 
     return render(request,'horario.html')
