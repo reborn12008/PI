e43ab7d1d754aef45bc84468ea380362f254442b
Rui Lebre <ruilebre@ua.pt>
2020-02-13 22:48:19 +0000

    acesso aos acessos do utilizador

--- horario/views.py
+++ horario/views.py
@@ -5,10 +5,16 @@ from keycode.source.models import Utilizador, Acesso
 
 def horario(request):
     username = request.user
-    print(username)
     current_user = Utilizador.objects.get(username=username)
-    #acesso = Acesso.objects.get(utilizador=current_user.id)
-    #for item in Acesso.objects.get(utilizador=current_user.id):
-       # print(item[0].id)
+    acessos = Acesso.objects.filter(utilizador=current_user.id)
+    
+    for acesso in acessos.iterator():
+        print(acesso.utilizador.username)
+        print(acesso.utilizador.tipo_utilizador)
+        print(acesso.horario.sala.nome)
+        print(acesso.horario.uc.nome_uc)
+        print(acesso.horario.dia_semana)
+        print(acesso.horario.hora_inicio)
+        print(acesso.horario.hora_fim)
 
     return render(request,'horario.html')
