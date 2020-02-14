18a2b22ba0466a1062f1c2e7814dc007598ad1f9
Filipe Jorge Cruz Rodrigues <filipe@filipe.com>
2020-02-14 16:21:45 +0000

    Adicionado bootstrap ao formulário de login

--- keycode/source/forms.py
+++ keycode/source/forms.py
@@ -2,7 +2,13 @@ from django import forms
 
 
 class loginForm(forms.Form):
-     username = forms.CharField(label='Username', max_length=20)
-     password = forms.CharField(label='Password',max_length=32, widget=forms.PasswordInput)
-
-
+     username = forms.CharField(label='Username', widget= forms.TextInput(
+          attrs = {
+          'class' : 'col-md-12',
+          }
+     ))
+     password = forms.CharField(label='Password', widget= forms.PasswordInput(
+          attrs = {
+               'class' : 'col-md-12',
+          }
+     ))
